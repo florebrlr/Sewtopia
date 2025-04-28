196646e434eeda8b4cc66fb4c5abd1608dacf6e9
Pink-Tea-Rex <flore.bruneliere.pro@gmail.com>
2025-04-28 19:23:19 +0200

    ✏️ ajout du component menu-icon-star + modif menu-item component

---
+++ src/app/menu-icon-star/menu-icon-star.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector:'menu-icon-star',
+  templateUrl:`./menu-icon-star.component.html`
+})
+
+export class MenuIconStarComponent {
+}
--- src/app/menu-item/menu-item.component.ts
+++ src/app/menu-item/menu-item.component.ts
@@ -1,11 +1,12 @@
 import { Component, input } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MenuIconStarComponent } from '../menu-icon-star/menu-icon-star.component';
 
 @Component({
   selector: 'menu-item',
   templateUrl: './menu-item.html',
   styleUrl: './menu-item.css',
-  imports: [RouterModule]
+  imports: [RouterModule,MenuIconStarComponent]
 })
 export class MenuItemComponent {
     label= input("")
