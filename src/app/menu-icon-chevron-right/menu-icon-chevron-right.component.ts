23c4ea2bd32aa28e88c2ae40d87719305b6c2c18
Pink-Tea-Rex <flore.bruneliere.pro@gmail.com>
2025-04-28 19:28:10 +0200

    ✏️ ajout component menu-icon-chevron-right + modif menu-item component

---
+++ src/app/menu-icon-chevron-right/menu-icon-chevron-right.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector:'menu-icon-chevron-right',
+  templateUrl:`./menu-icon-chevron-right.component.html`
+})
+
+export class MenuIconChevronRightComponent {
+}
