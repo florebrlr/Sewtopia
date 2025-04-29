ed73591d1b36e8d49ec0d4c6a326ef6f24d4432f
faustine.godbillot <prestataire.0175243@ouest-france.fr>
2025-04-29 12:33:57 +0200

    🎨 organize css per component & import images correctly

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
-import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  imports: [HeaderComponent,FooterComponent,SidebarComponent],
+  styleUrl: './home-page.component.css',
 })
 export class HomePageComponent {}
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { MenuItemComponent } from '../menu-item/menu-item.component';
 @Component({
   selector: 'app-sidebar',
-  templateUrl: './sidebar.compoment.html',
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css',
   imports: [MenuItemComponent],
 })
 export class SidebarComponent {}
